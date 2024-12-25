feeb99c6b28aec48507140c9646bba387ae71716
mahimnaupadhyaysvit <24mca39@svitvasad.ac.in>
2024-12-25 13:56:57 +0530

    laravel example-app, worked on basic routing

---
+++ example-app/resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+        <nav class='flex w-full h-[100px] justify-between items-center p-5 text-2xl'>
+            <a href='/'>Home</a>
+            <a href='/aboutus'>About</a>
+            <a href='/contactus'>Contact</a>
+        </nav>
+
+        <div class='flex w-full h-screen justify-center items-center'>
+            <h1 class='text-2xl'>Hi from home page</h1>
+        </div>
+</body>
+</html>
---
+++ example-app/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/aboutus', function () {
+    return view('about');
+});
+
+Route::get('/contactus', function () {
+    return view('contact');
+});
